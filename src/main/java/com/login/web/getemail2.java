e0adae97414e760db27bece60803f26bd6babcf2
vijayDVA <48407258+vijayDVA@users.noreply.github.com>
2021-03-22 15:47:51 +0530

    commit6

--- src/main/java/com/login/web/getemail2.java
+++ src/main/java/com/login/web/getemail2.java
@@ -33,8 +33,8 @@ public class getemail2 {
 	public String getemail() throws SQLException
 	{
 		String gmail;
-		loginBean.setUname(uname);
-		loginBean.setPassword(passwd);
+		loginBean.setUname("dva");
+		loginBean.setPassword("dva");
 		
 		gmail = loginDao.GetEmail(loginBean);
 		
